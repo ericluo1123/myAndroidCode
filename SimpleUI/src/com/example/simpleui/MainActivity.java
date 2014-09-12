91b96404f54a99c19a7a91c78ef2c3bf757e4204
FrankLikuohao <frank.likuohao@gmail.com>
2014-09-12 22:05:14 +0800

    2014/0912/1004
    lesson end record

--- SimpleUI/src/com/example/simpleui/MainActivity.java
+++ SimpleUI/src/com/example/simpleui/MainActivity.java
@@ -1,25 +1,93 @@
 package com.example.simpleui;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
 	private EditText editText;
 	private Button button;
-
+	private CheckBox checkBox;
+	private Editor editor;
+	private SharedPreferences sp;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		sp = getSharedPreferences("settings", Context.MODE_PRIVATE);
+		editor = sp.edit(); 
+		
 		editText = (EditText) findViewById(R.id.editText1);
 		button = (Button) findViewById(R.id.button1);
+		checkBox =(CheckBox) findViewById(R.id.checkBox1);
+		
+		button.setText("Send");
+		button.setOnClickListener(new OnClickListener(){
 
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Log.d("debug", "click from button1");
+				sendText();
+			}
+			
+		});
+		
+		editText.setOnKeyListener(new OnKeyListener() {
+			
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				// TODO Auto-generated method stub
+				Log.d("debug", "keyCode: " + keyCode);
+				
+				String text = editText.getText().toString();
+				editor.putString("text", text);
+				editor.commit();
+				
+				if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+					sendText();
+				}
+				
+
+				return false;
+			}
+		});
+		
+		editText.setText(sp.getString("text", ""));
+	}
+	
+	public void ClickButton2(View view) {
+	
+		Log.d("debug", "click from button2");
+		sendText();
+
+	}
+	
+	public void sendText(){
+		String text = editText.getText().toString();
+		if(checkBox.isChecked()){
+			text="********";
+		}
+		
+		
+		Toast.makeText(this, text, Toast.LENGTH_LONG).show();//shwo the text to a box and time
+		editText.setText("");
 	}
 
 	@Override
