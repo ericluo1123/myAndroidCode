81d1304f91a94a38b43e0a8918baece190fb38ae
eric <eric.luo1123@gmail.com>
2014-09-25 16:21:33 +0800

    2014/0925/1621

--- myAndroidProject/src/com/example/myandroidproject/MainActivity.java
+++ myAndroidProject/src/com/example/myandroidproject/MainActivity.java
@@ -4,15 +4,46 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
+
+	private Button button;
+	private TextView textView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		textView = (TextView) findViewById(R.id.textView1);
+		textView.setText("my second mobild app");
+		
+		
+		
+		button=(Button) findViewById(R.id.button1);
+		button.setText("show");
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				textView.setText("hello world!");
+			}
+		});
+		
+
+
 	}
 
+	
+	
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
